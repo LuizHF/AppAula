aef9ae4776840be9428fedd800ec35cfab08e863
Luiz Henrique <luizhenriquefurtado2015@gmail.com>
2022-04-20 21:42:34 -0400

    Adicionado os Botoes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,8 @@ export class AppComponent {
     { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
     { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
     { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+    { title: 'Navegação', url: '/navegacao', icon: 'code' },
+    { title: 'Navegação2', url: '/navegacao2', icon: 'code' },
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
