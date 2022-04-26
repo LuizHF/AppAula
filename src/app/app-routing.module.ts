9fae44f070bab5198f069009412346800e8c54db
Luiz Henrique <luizhenriquefurtado2015@gmail.com>
2022-04-25 20:24:57 -0400

    Atividade dia 20

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'home',
     pathMatch: 'full'
   },
   {
@@ -18,6 +18,34 @@ const routes: Routes = [
   {
     path: 'navegacao2',
     loadChildren: () => import('./pages/navegacao2/navegacao2.module').then( m => m.Navegacao2PageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'botao',
+    loadChildren: () => import('./pages/botao/botao.module').then( m => m.BotaoPageModule)
+  },
+  {
+    path: 'botao1',
+    loadChildren: () => import('./pages/botao1/botao1.module').then( m => m.Botao1PageModule)
+  },
+  {
+    path: 'botao2',
+    loadChildren: () => import('./pages/botao2/botao2.module').then( m => m.Botao2PageModule)
+  },
+  {
+    path: 'botao3',
+    loadChildren: () => import('./pages/botao3/botao3.module').then( m => m.Botao3PageModule)
+  },
+  {
+    path: 'botao4',
+    loadChildren: () => import('./pages/botao4/botao4.module').then( m => m.Botao4PageModule)
+  },
+  {
+    path: 'botao5',
+    loadChildren: () => import('./pages/botao5/botao5.module').then( m => m.Botao5PageModule)
   }
 ];
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,12 @@ export class AppComponent {
     { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
     { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
     { title: 'Navegação', url: '/navegacao', icon: 'code' },
+    { title: 'Botões', url: '/botao', icon: 'construct' },
+    { title: 'Botão1', url: '/botao1', icon: 'construct' },
+    { title: 'Botão2', url: '/botao2', icon: 'construct' },
+    { title: 'Botão3', url: '/botao3', icon: 'construct' },
+    { title: 'Botão4', url: '/botao4', icon: 'construct' },
+    { title: 'Botão5', url: '/botao5', icon: 'construct' },
     { title: 'Navegação2', url: '/navegacao2', icon: 'code' },
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
